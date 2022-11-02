b03f205d5fc2c483b5ab48634760be36541a7c3d
BDXGaming <jx.welsko@gmail.com>
2022-11-02 10:37:57 -0400

    Update test script

--- test.py
+++ test.py
@@ -2,4 +2,7 @@ print("This is a python file")
 print("This is a python file line 2")
 print("This is from my python interperter")
 x = "Hello World"
+x_2 = "Test"
 print(x)
+if True:
+    print(x_2)
